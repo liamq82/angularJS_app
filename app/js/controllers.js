f9b48a834ad4ae4979bfcbaa75bdea2b42dc2134
liamq82 <liamq82@gmail.com>
2014-05-15 11:52:48 +0100

    rendered rest resource as table
    Used ng-repeat to render the get request of type jeans as a table.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -19,24 +19,11 @@ angular.module('myApp.controllers', [])
         }
     ]).controller('ItemController', ['$scope', '$routeParams', 'Inventory', 'Item',
         function($scope, $routeParams, Inventory, Item) {
-            // $scope.items = [{}];
-            /*$scope.items = [{
-                type: 'jeans',
-                leg: 32,
-                waist: 32,
-                color: 'blue'
-            }, {
-                type: 'jeans',
-                leg: 28,
-                waist: 28,
-                color: 'blue'
-            }];*/
             $scope.retrievedItems = Inventory.get({
                 type: $routeParams.type
             }, function(items) {
-/*                $scope.test= items[0].item1.type;
                 $scope.items = items;
-                console.log(items[0]);*/
+                $scope.type = $routeParams.type;
             });
         }
     ]);
