d6dfe82436bbefe9b9c0b84dcc1a885be0ddfe1b
liamq82 <liamq82@gmail.com>
2014-05-20 13:17:32 +0100

    End to end tests.
    Added end to end tests for the inventory page to test adding items are
    then added to the inventory table.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -23,8 +23,73 @@ describe('my app', function() {
             toMatch(/Inventory/);
         });
 
-        it('should populate the item type in the InventoryController model on submit', function() {
+        it('should add one item to the inventory table on submit', function() {
+            element(by.model('item.type')).sendKeys('jeans');
+            element(by.model('item.style')).sendKeys('tight');
+            element(by.model('item.description')).sendKeys('tight fitting blue jeans');
+            element(by.model('item.color')).sendKeys('blue');
+            element(by.model('item.price')).sendKeys('25.99');
+            element(by.model('item.quantity')).sendKeys('5');
+            element(by.buttonText('Submit')).click();
 
+            expect(element(by.css('.type')).getText()).toBe('jeans');
+            expect(element(by.css('.style')).getText()).toBe('tight');
+            expect(element(by.css('.description')).getText()).toBe('tight fitting blue jeans');
+            expect(element(by.css('.color')).getText()).toBe('blue');
+            expect(element(by.css('.price')).getText()).toBe('25.99');
+            expect(element(by.css('.quantity')).getText()).toBe('5');
+        });
+
+        it('should add multiple items to the inventory table with multiple submits', function() {
+            element(by.model('item.type')).sendKeys('jeans');
+            element(by.model('item.style')).sendKeys('tight');
+            element(by.model('item.description')).sendKeys('tight fitting blue jeans');
+            element(by.model('item.color')).sendKeys('blue');
+            element(by.model('item.price')).sendKeys('25.99');
+            element(by.model('item.quantity')).sendKeys('5');
+            element(by.buttonText('Submit')).click();
+
+            element(by.model('item.type')).sendKeys('t-shirt');
+            element(by.model('item.style')).sendKeys('loose');
+            element(by.model('item.description')).sendKeys('loose fitting t-shirt');
+            element(by.model('item.color')).sendKeys('red');
+            element(by.model('item.price')).sendKeys('9.99');
+            element(by.model('item.quantity')).sendKeys('10');
+            element(by.buttonText('Submit')).click();
+
+            element.all(by.css('.type')).then(function(items) {
+                expect(items.length).toBe(2);
+                expect(items[0].getText()).toBe('jeans');
+                expect(items[1].getText()).toBe('t-shirt');
+            });
+
+            element.all(by.css('.style')).then(function(items) {
+                expect(items.length).toBe(2);
+                expect(items[0].getText()).toBe('tight');
+                expect(items[1].getText()).toBe('loose');
+            });
+
+            element.all(by.css('.description')).then(function(items) {
+                expect(items.length).toBe(2);
+                expect(items[0].getText()).toBe('tight fitting blue jeans');
+                expect(items[1].getText()).toBe('loose fitting t-shirt');
+            });
+
+            element.all(by.css('.color')).then(function(items) {
+                expect(items.length).toBe(2);
+                expect(items[0].getText()).toBe('blue');
+                expect(items[1].getText()).toBe('red');
+            });
+            element.all(by.css('.price')).then(function(items) {
+                expect(items.length).toBe(2);
+                expect(items[0].getText()).toBe('25.99');
+                expect(items[1].getText()).toBe('9.99');
+            });
+            element.all(by.css('.quantity')).then(function(items) {
+                expect(items.length).toBe(2);
+                expect(items[0].getText()).toBe('5');
+                expect(items[1].getText()).toBe('10');
+            });
         });
 
     });
